80b203edc9e7bd4075ca05093bac6873d02132da
Hhurtadoabcx <contacthurtadolxyz@gmail.com>
2024-03-24 16:59:46 -0400

    Expo Gyroscope integration

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Gyroscope } from 'expo-sensors';
 
 export default function App() {
+  const [data, setData] = useState({ x: 0, y: 0, z: 0 });
+
+  useEffect(() => {
+    _subscribe();
+
+    return () => {
+      _unsubscribe();
+    };
+  }, []);
+
+  const _subscribe = () => {
+    Gyroscope.addListener((gyroscopeData) => {
+      setData(gyroscopeData);
+    });
+    Gyroscope.setUpdateInterval(1000); // Intervalo de actualización
+  };
+
+  const _unsubscribe = () => {
+    Gyroscope.removeAllListeners();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Gyroscope Data:</Text>
+      <Text>X: {data.x.toFixed(2)}</Text>
+      <Text>Y: {data.y.toFixed(2)}</Text>
+      <Text>Z: {data.z.toFixed(2)}</Text>
     </View>
   );
 }
